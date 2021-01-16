2a95d489227359bb71397cdfb6f6cece6cee01c2
saicharan shivarathri <saicharanshivarathr@gmail.com>
2021-01-16 17:39:29 +0530

    Add project files.

---
+++ corewebapi/Model/CourseContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace corewebapi.Model
+{
+    public class CourseContext : DbContext
+    {
+        // Replace your connecting string
+        readonly string connectionstring = "Server=tcp:demoapi.database.windows.net,1433;Initial Catalog=demoapi;Persist Security Info=False;User ID=demoapi;Password=temp!123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(connectionstring);
+            base.OnConfiguring(options);
+        }
+    }
+
+    
+}
